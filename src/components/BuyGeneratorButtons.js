d01c7f5ce9a95aafb651cf526a14778230cd3704
Nam Doan <nam.quang.doan@gmail.com>
2020-10-18 18:59:33 -0400

    Cost for buying calculated correctly

--- src/components/BuyGeneratorButtons.js
+++ src/components/BuyGeneratorButtons.js
@@ -1,19 +1,41 @@
 import React from 'react';
 
-const BuyGeneratorButtons = (props) => {
+const BuyGeneratorButtons = (props) => {	
 	const updatePerSecond = () => {
 		const totalPerSecond = props.generators[1]
 		props.setPerSecond(totalPerSecond)
 	}
 	
-	const increaseGenerator = (type, amount) => {
+	const genOneTotalCost = (type, amount) => {
+		const current_gen_num = props.generators[type]
+		
+		const genOneCost = (number) => {
+			return Math.pow(10, 1+0.05*number)
+		}
+		if (amount === 1){
+			return Math.round(genOneCost(current_gen_num))
+		} else {
+			return Math.round([...Array(amount).keys()].map(x => genOneCost(x+current_gen_num)).reduce((total, x) => total + x))
+		}
+		
+	}
+	
+	const increaseGenerator = (type, amount) => {	
 		if (type === 1) {
-			if (props.coins >= 10*amount){
-			props.setCoins (props.coins - 10*amount)
-			props.setGenerators ({...props.generators, 1:props.generators[1]+amount})
-			updatePerSecond()
+			if (amount !== 1){
+				//Get total cost of increase gen[type] by amount
+				const gen_total_cost = genOneTotalCost(type, amount)
+			}
+			else {
+				const gen_total_cost = genOneTotalCost(type, amount)
+				if (props.coins >= gen_total_cost){
+						props.setCoins (props.coins - gen_total_cost)
+						props.setGenerators ({...props.generators, 1:props.generators[1]+1})
+						updatePerSecond()
+				}
 			}
 		}
+		
 	}
 	
 	return (
@@ -22,7 +44,7 @@ const BuyGeneratorButtons = (props) => {
 			<button onClick = {() => increaseGenerator(1,props.buyAmount)}>
 				Buy Generator ({props.generators[1]})
 				<br/>
-				Cost: 10
+				Cost: {genOneTotalCost(1, props.buyAmount)}
 			</button>
 		</>
 	)
